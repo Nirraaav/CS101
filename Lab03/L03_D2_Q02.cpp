c18d8e946cb0c1327e41baf10229e91ccb745132
Nirraaav <niravbhattad24@gmail.com>
2024-01-23 22:18:32 +0530

    Lab03 Question 2

--- Lab03/L03_D2_Q02.cpp
+++ Lab03/L03_D2_Q02.cpp
@@ -48,36 +48,81 @@ main_program{
 
 	// Starfish
 
-	left(72);
-	forward(500);
-	right(144);
-	forward(500);
-	right(144);
-	forward(500);
-	right(144);
-	forward(500);
-	right(144);
-	forward(500);
-	right(144);
-	// wait(5);
-	penUp();
-	forward(250);
-	right(72);
-	forward(45);
-	penDown();
-	forward(25);
-	penUp();
-	forward(25);
-	penDown();
-	forward(25);
+	// left(72);
+	// forward(500.0/3);
+	// penUp();
+	// forward(500.0/3);
+	// penDown();
+	// forward(500.0/3);
+	// right(144);
+	// forward(500.0/3);
+	// penUp();
+	// forward(500.0/3);
+	// penDown();
+	// forward(500.0/3);
+	// right(144);
+	// forward(500.0/3);
+	// penUp();
+	// forward(500.0/3);
+	// penDown();
+	// forward(500.0/3);
+	// right(144);
+	// forward(500.0/3);
+	// penUp();
+	// forward(500.0/3);
+	// penDown();
+	// forward(500.0/3);
+	// right(144);
+	// forward(500.0/3);
+	// penUp();
+	// forward(500.0/3);
+	// penDown();
+	// forward(500.0/3);
+	// right(144);
+	// // wait(5);
+	// penUp();
+	// forward(250);
+	// right(72);
+	// forward(45);
+	// penDown();
+	// forward(25);
+	// penUp();
+	// forward(25);
+	// penDown();
+	// forward(25);
+
+	// penUp();
+	// right(90);
+	// forward(50);
+	// penDown();
+	// repeat(10000){
+	// 	forward((37.5*PI)/10000);
+	// 	right(180.0/10000);
+	// }
+	// wait(10);
+
+	// Mickey Mouse hehehehe
+
+	Circle Face = Circle(750, 750, 200);
+	Face.paint();
+	Circle Ear1 = Circle(750+125*sqrt(2), 750-125*sqrt(2), 50);
+	Ear1.paint();
+	Circle Ear2 = Circle(750-125*sqrt(2), 750-125*sqrt(2), 50);
+	Ear2.paint();
+	Circle Eye1 = Circle(750+50*sqrt(2), 750-50*sqrt(2), 25);
+	Eye1.paint();
+	Circle Eye2 = Circle(750-50*sqrt(2), 750-50*sqrt(2), 25);
+	Eye2.paint();
 
 	penUp();
+	forward(65);
 	right(90);
-	forward(50);
+	forward(75);
+	right(30);
 	penDown();
-	repeat(10000){
-		forward((37.5*PI)/10000);
-		right(180.0/10000);
+	repeat(1000){
+		forward((50*PI)/1000);
+		right(120.0/1000);
 	}
-	wait(10);
+	wait(5);
 }
