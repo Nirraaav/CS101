54bce14cb21f8cbd6644943523b5367814868b22
Nirraaav <niravbhattad24@gmail.com>
2024-01-23 21:53:31 +0530

    Lab03 New Solutions

--- Lab03/L03_D2_Q02.cpp
+++ Lab03/L03_D2_Q02.cpp
@@ -1,44 +1,83 @@
 #include <simplecpp>
 
 main_program{
-	turtleSim("Turtle");
-	penUp();
-	forward(-250);
-	right(90);
-	forward(250);
-	left(90);
-	penDown();
-	repeat(4){
-		forward(500);
-		left(90);
-	}
-	penUp();
-	left(90);
+	turtleSim("A mythical creature from IITB");
+	// penUp();
+	// forward(-250);
+	// right(90);
+	// forward(250);
+	// left(90);
+	// penDown();
+	// repeat(4){
+	// 	forward(500);
+	// 	left(90);
+	// }
+	// penUp();
+	// left(90);
+	// forward(500);
+	// right(90);
+	// forward(250);
+	// left(90);
+	// penDown();
+	// Circle C = Circle(750, 300, 200);
+	// C.paint();
+	// left(90);
+	// forward(125);
+	// right(90);
+	// penUp();
+	// forward(100);
+	// forward(150);
+	// right(90);
+	// forward(50);
+	// penDown();
+	// forward(50);
+	// penUp();
+	// forward(50);
+	// penDown();
+	// forward(50);
+	// penUp();
+	// right(90);
+	// forward(100);
+	// penDown();
+	// forward(25);
+	// right(90);
+	// forward(150);
+	// right(90);
+	// forward(25);
+	// wait(5);
+
+	// Starfish
+
+	left(72);
 	forward(500);
-	right(90);
-	forward(125);
-	left(90);
-	penDown();
-	repeat(4){
-		forward(250);
-		right(90);
-	}
+	right(144);
+	forward(500);
+	right(144);
+	forward(500);
+	right(144);
+	forward(500);
+	right(144);
+	forward(500);
+	right(144);
+	// wait(5);
 	penUp();
-	forward(150);
-	right(90);
-	forward(50);
+	forward(250);
+	right(72);
+	forward(45);
 	penDown();
-	forward(50);
+	forward(25);
 	penUp();
-	forward(50);
-	penDown();
-	forward(50);
-	penUp();
-	right(90);
-	forward(75);
-	right(90);
 	forward(25);
 	penDown();
-	forward(100);
-	wait(5);
+	forward(25);
+
+	penUp();
+	right(90);
+	forward(50);
+	penDown();
+	repeat(10000){
+		forward((37.5*PI)/10000);
+		right(180.0/10000);
+	}
+	wait(10);
 }
--- Lab03/L03_D2_Q03.cpp
+++ Lab03/L03_D2_Q03.cpp
@@ -3,18 +3,14 @@
 main_program{
 	int n;
 	cin >> n;
-	int a[n];
-	for(int i = 0; i < n; i++){
-		cin >> a[i];
-	}
 	int b;
 	cin >> b;
-	for(int i = 0; i < n; i++){
-		a[i] %= b;
-	}
 	int ans = 1;
-	for(int i = 0; i < n; i++){
-		ans *= a[i];
+	repeat(n){
+		int a;
+		cin >> a;
+		a %= b;
+		ans *= a;
 		ans %= b;
 	}
 	cout << ans << "\n";
