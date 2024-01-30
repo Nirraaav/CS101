07a64b6ce30665b5e23be590021fc65034bf83a8
Nirraaav <niravbhattad24@gmail.com>
2024-01-31 02:51:45 +0530

    Lab04 return error

--- Lab04/L04_D2_Q02.cpp
+++ Lab04/L04_D2_Q02.cpp
@@ -5,36 +5,36 @@ main_program{
 	cin >> d >> m >> y;
 	if((y < 1400) || (y > 3000)){
 		cout << "invalid" << "\n";
-		return;
+		return 0;
 	}
 	if((d > 31) || (d < 1)){
 		cout << "invalid" << "\n";
-		return;
+		return 0;
 	}
 	if((m > 12) || (m < 1)){
 		cout << "invalid" << "\n";
-		return;
+		return 0;
 	}
 	if(!((m == 1) || (m == 3) || (m == 5) || (m == 7) || (m == 8) || (m == 10) || (m == 12))){
 		if((d > 30) || (d < 1)){
 			cout << "invalid" << "\n";
-			return;
+			return 0;
 		}
 	}
 	if((y % 4 == 0) && !(y % 400 == 0) && (y % 100 == 0)){
 		if((m == 2) && (d == 29)){
 			cout << "invalid" << "\n";
-			return;
+			return 0;
 		}
 	}
 	if((y % 4 == 0) && (m == 2) && ((d < 1) || (d > 29))){
 		cout << "invalid" << "\n";
-		return;
+		return 0;
 	}
 	if(!(y % 4 == 0) && (m == 2) && ((d < 1) || (d > 28))){
 		cout << "invalid" << "\n";
-		return;
+		return 0;
 	}
 	cout << "valid" << "\n";
-	return;
+	return 0;
 }
