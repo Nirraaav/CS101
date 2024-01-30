6eec0f29dda57cd3f2ed52fe0c28d752dc816b97
Nirraaav <niravbhattad24@gmail.com>
2024-01-31 02:47:59 +0530

    Lab04

--- Lab04/L04_D2_Q02.cpp
+++ Lab04/L04_D2_Q02.cpp
@@ -5,36 +5,36 @@ main_program{
 	cin >> d >> m >> y;
 	if((y < 1400) || (y > 3000)){
 		cout << "invalid" << "\n";
-		return 0;
+		return;
 	}
 	if((d > 31) || (d < 1)){
 		cout << "invalid" << "\n";
-		return 0;
+		return;
 	}
 	if((m > 12) || (m < 1)){
 		cout << "invalid" << "\n";
-		return 0;
+		return;
 	}
 	if(!((m == 1) || (m == 3) || (m == 5) || (m == 7) || (m == 8) || (m == 10) || (m == 12))){
 		if((d > 30) || (d < 1)){
 			cout << "invalid" << "\n";
-			return 0;
+			return;
 		}
 	}
 	if((y % 4 == 0) && !(y % 400 == 0) && (y % 100 == 0)){
 		if((m == 2) && (d == 29)){
 			cout << "invalid" << "\n";
-			return 0;
+			return;
 		}
 	}
 	if((y % 4 == 0) && (m == 2) && ((d < 1) || (d > 29))){
 		cout << "invalid" << "\n";
-		return 0;
+		return;
 	}
 	if(!(y % 4 == 0) && (m == 2) && ((d < 1) || (d > 28))){
 		cout << "invalid" << "\n";
-		return 0;
+		return;
 	}
 	cout << "valid" << "\n";
-	return 0;
+	return;
 }
--- Lab04/L04_D2_Q03.cpp
+++ Lab04/L04_D2_Q03.cpp
@@ -3,7 +3,20 @@
 main_program{
 	int n;
 	cin >> n;
-	if((n == 6) || (n == 28) || (n == 496) || (n == 8128)){
+	// if((n == 6) || (n == 28) || (n == 496) || (n == 8128)){
+	// 	cout << "perfect" << "\n";
+	// }else{
+	// 	cout << "not perfect" << "\n";
+	// }
+	int sum = 0;
+	int curr = 1;
+	repeat(n){
+		if(n % curr == 0){
+			sum += curr;
+		}
+		curr++;
+	}
+	if(sum == 2*n){
 		cout << "perfect" << "\n";
 	}else{
 		cout << "not perfect" << "\n";
