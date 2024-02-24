82b815aed2b0b8fbbb18ec8160f18a13da0224db
Nirraaav <niravbhattad24@gmail.com>
2024-02-24 19:21:00 +0530

    Lab06 D2 Q3 Comment

--- Lab06/L06_D2_Q03.cpp
+++ Lab06/L06_D2_Q03.cpp
@@ -11,7 +11,7 @@ long double root(long double &a, long double &b, long double &c, long double &m,
 	if(m == n){
 		return m;
 	}
-	cout << m << " " << n << "\n";
+	// cout << m << " " << n << "\n";
 	if(abs(f(a, b, c, mid)) < eps){
 		return mid;
 	}else if(f(a, b, c, mid)*f(a, b, c, m) < 0){
@@ -28,7 +28,7 @@ main_program{
 	long double m, n;
 	cin >> a >> b >> c;
 	cin >> m >> n;
-	long double eps = 0.01;
+	long double eps = 0.0001;
 	long double ans = root(a, b, c, m, n, eps);
 	cout << ans << "\n";
 }
