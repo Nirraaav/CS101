d6f1200e01c2491f6238e2b7e8159478d45cefbf
Nirav <niravbhattad24@gmail.com>
2024-03-26 00:25:07 +0530

    Sliding Puzzle

--- Lab09/L09_D1_Q01.cpp
+++ Lab09/L09_D1_Q01.cpp
@@ -1,39 +1,39 @@
 #include <simplecpp>
 
 main_program {
-    long long int n;
-    cin >> n;
-    long long int a[n][n];
-    for (long long int i = 0; i < n; i++) {
-        for (long long int j = 0; j < n; j++) {
-            cin >> a[i][j];
-        }
-    }
-    long long int top = 0, bottom = n - 1, left = 0, right = n - 1;
-    while (top <= bottom && left <= right) {
-        for (long long int j = left; j <= right; j++) {
-            cout << a[top][j] << " ";
-        }
-        top++;
+	long long int n;
+	cin >> n;
+	long long int a[n][n];
+	for (long long int i = 0; i < n; i++) {
+		for (long long int j = 0; j < n; j++) {
+			cin >> a[i][j];
+		}
+	}
+	long long int top = 0, bottom = n - 1, left = 0, right = n - 1;
+	while (top <= bottom && left <= right) {
+		for (long long int j = left; j <= right; j++) {
+			cout << a[top][j] << " ";
+		}
+		top++;
 
-        for (long long int i = top; i <= bottom; i++) {
-            cout << a[i][right] << " ";
-        }
-        right--;
+		for (long long int i = top; i <= bottom; i++) {
+			cout << a[i][right] << " ";
+		}
+		right--;
 
-        if (top <= bottom) {
-            for (long long int j = right; j >= left; j--) {
-                cout << a[bottom][j] << " ";
-            }
-            bottom--;
-        }
+		if (top <= bottom) {
+			for (long long int j = right; j >= left; j--) {
+				cout << a[bottom][j] << " ";
+			}
+			bottom--;
+		}
 
-        if (left <= right) {
-            for (long long int i = bottom; i >= top; i--) {
-                cout << a[i][left] << " ";
-            }
-            left++;
-        }
-    }
-    cout << "\n";
+		if (left <= right) {
+			for (long long int i = bottom; i >= top; i--) {
+				cout << a[i][left] << " ";
+			}
+			left++;
+		}
+	}
+	cout << "\n";
 }
